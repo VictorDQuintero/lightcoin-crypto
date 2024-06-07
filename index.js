522eb125c2b7e6ddb017634b7a9fb3a37ff1936b
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-07 15:23:50 -0600

    Add keeping up with transactions functionality

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ class Account {
   constructor(username) {
     this.username = username;
     // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+    this.transactions = [];
   }
 
   get balance() {
     // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
   }
 
   addTransaction(transaction) {
@@ -25,18 +30,19 @@ class Transaction {
     this.time = new Date();
     // Add the transaction to the account
     this.account.addTransaction(this);
+    //as an example it calls t1.account.addTransaction() and puts the whole object (t1) in it. And because this.account is assigned myAccount it effectively calls myAccount.addTransaction(t1) // Clever!
   }
 }
 
 class Withdrawal extends Transaction {
   get value() {
-    return -this.amount;
+    return -this.amount; // returns a negative ammount to any new withdrawal object as tx.value
   }
 }
 
 class Deposit extends Transaction {
   get value() {
-    return this.amount;
+    return this.amount; // returns a positive ammount to any new deposit object as tx.value
   }
 }
 
@@ -47,9 +53,18 @@ const myAccount = new Account("billybob");
 console.log("Starting Balance:", myAccount.balance);
 
 const t1 = new Deposit(120.0, myAccount);
+console.log(t1.value);
+console.log(t1);
 t1.commit();
 
+console.log(myAccount.transactions);
+console.log(myAccount.balance);
+
 const t2 = new Withdrawal(50.0, myAccount);
+console.log(t2.value);
+console.log(t2);
 t2.commit();
+console.log(myAccount.transactions);
+console.log(myAccount.balance);
 
 console.log("Ending Balance:", myAccount.balance);
